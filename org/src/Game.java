1351c503cd7e82799a9459023c111a84f9edaeee
christinaqiu3 <christinaqiu3@gmail.com>
2023-08-28 10:34:51 -0400

    fixed errors yay done!

--- org/src/Game.java
+++ org/src/Game.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 public class Game {
 
     public static void main(String[] args) {
-        Runnable game = new org.cis1200.RunDungeon();
+        Runnable game = new RunDungeon();
 
         SwingUtilities.invokeLater(game);
     }
